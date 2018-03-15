46cc4083b7ae2101b4248fd3044b8dd77d5fff1d
Ubuntu <ubuntu@ip-172-31-28-120.us-west-1.compute.internal>
2018-03-15 19:57:22 +0000

    tuning logs

--- scripts/run_hyperparameter_training.sh
+++ scripts/run_hyperparameter_training.sh
@@ -3,5 +3,5 @@ min_filter=$2
 max_filter=$3
 python dragonn_hyperparameter_tuning.py ../processed_data/tss_positives.fasta \
 ../processed_data/tss_negatives.fasta 150 $num_layer $min_filter $max_filter \
-0.2 0.2 50 > hyperparameter_log_layer$num_layer_minfilter$min_filter_maxfilter$max_filter.txt
+0.2 0.2 50 > hyperparameter_log_layer${num_layer}_minfilter${min_filter}_maxfilter${max_filter}.txt
 
