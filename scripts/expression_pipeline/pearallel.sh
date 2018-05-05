17d7b1f48497d04004ee1d04e4ed9822f6727432
Kimberly <kiminsigne@gmail.com>
2018-05-05 16:55:33 -0700

    reorganize and final mapping fixes

--- scripts/expression_pipeline/pearallel.sh
+++ scripts/expression_pipeline/pearallel.sh
@@ -2,6 +2,6 @@ prefix=$1
 output=$2
 
 echo "Aligning sample $prefix"
-pear -j 30 --max-uncalled-base 0 -f *$prefix*R2* -r *$prefix*R1* -o ${prefix}.paired
+pear -j 30 --max-uncalled-base 0 -f $prefix*R2* -r $prefix*R1* -o ${prefix}.paired
 
 mv ${prefix}.paired* $output
