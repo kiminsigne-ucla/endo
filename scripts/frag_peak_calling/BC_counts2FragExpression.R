0796e8fb8304ff323b06b8d7f410146c804b3012
Kimberly <kiminsigne@gmail.com>
2018-05-05 18:46:33 -0700

    frag pipeline

---
+++ scripts/frag_peak_calling/BC_counts2FragExpression.R
@@ -0,0 +1,54 @@
+library("dplyr")
+options(stringsAsFactors = F)
+
+#Read in all Sequencing data for rLP5-Frag
+
+filelist = list.files(pattern = '*.txt')
+for(i in filelist) {
+  x <- read.table(i, col.names=c(i, 'barcode'), header = F)
+  x[[i]] <- 1000000*x[[i]]/sum(x[[i]])  #Normalizes by RPM
+  assign(i,x)
+}
+
+
+rLP5_frag <- full_join(rLP5_frag_DNA1_1.txt, rLP5_frag_DNA1_2.txt, by='barcode') %>%
+  full_join(., rLP5_frag_DNA2_1.txt, by='barcode') %>%
+  full_join(., rLP5_frag_DNA2_2.txt, by='barcode') %>%
+  full_join(., rLP5_frag_RNA1_1.txt, by='barcode') %>%
+  full_join(., rLP5_frag_RNA1_2.txt, by='barcode') %>%
+  full_join(., rLP5_frag_RNA2_1.txt, by='barcode') %>%
+  full_join(., rLP5_frag_RNA2_2.txt, by='barcode') 
+
+
+names(rLP5_frag) = sub(".txt","", names(rLP5_frag)) #rename all colummns that were named after text file temp <- frag_Data[c(-1,-5)]
+
+rm(list = c(filelist))
+rm(x)
+
+rLP5_frag[is.na(rLP5_frag)] <- 0
+
+mapped_fragstats <- read.table("../ref/FragStats.txt", header = F, fill=T, col.names = c('barcode', 'start', 'end', 'strand', 'fragment'))
+mapped_fragstats <- mapped_fragstats[!duplicated(mapped_fragstats$barcode),]
+
+fragstats <- left_join(mapped_fragstats, rLP5_frag, by ='barcode')
+
+Frag_LB <- fragstats %>%
+  group_by(fragment) %>% 
+  mutate(num_mapped_barcodes = n()) %>%
+  filter(rLP5_frag_DNA1_1 > 0 | rLP5_frag_DNA1_2 > 0 | rLP5_frag_DNA2_1 > 0 | rLP5_frag_DNA2_2 > 0) %>%
+  mutate(num_integrated_barcodes = n()) %>%
+  filter(num_integrated_barcodes >= 1) %>%
+  mutate(RNA_exp_1 = (sum(rLP5_frag_RNA1_1)+sum(rLP5_frag_RNA1_2))/(sum(rLP5_frag_DNA1_1)+sum(rLP5_frag_DNA1_2)),
+         RNA_exp_2 = (sum(rLP5_frag_RNA2_1)+sum(rLP5_frag_RNA2_2))/(sum(rLP5_frag_DNA2_1)+sum(rLP5_frag_DNA2_2)),
+         RNA_exp_ave = ((RNA_exp_1 + RNA_exp_2)/2),
+         DNA_sum_1 = sum(rLP5_frag_DNA1_1)+sum(rLP5_frag_DNA1_2),
+         DNA_sum_2 = sum(rLP5_frag_DNA1_1)+sum(rLP5_frag_DNA1_2),
+         DNA_ave = ((DNA_sum_1 + DNA_sum_2)/2)) %>% 
+  ungroup() %>% 
+  filter(RNA_exp_1 >= .1 & RNA_exp_2 >= .1, RNA_exp_1 < 10000 & RNA_exp_2 < 10000, DNA_ave>1) %>% #Filter out fragments with low expression of 
+  mutate(variation = abs(log2(RNA_exp_2/RNA_exp_1))) %>% 
+  filter(variation < 2.32) %>% #Remove fragments that have a 5x difference between biological replicates
+  select(fragment, RNA_exp_1, RNA_exp_2, RNA_exp_ave, DNA_sum_1, DNA_sum_2, DNA_ave, num_mapped_barcodes, num_integrated_barcodes, start, end, strand, variation) %>% 
+  distinct() 
+
+write.table(Frag_LB, "./U00096.2_frag-rLP5_LB_expression.txt", quote = F, row.names = F)
