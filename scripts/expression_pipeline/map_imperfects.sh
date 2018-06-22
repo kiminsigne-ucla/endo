8060dfd781361e2e6f5e37461590b50b5ed053cf
kinsigne <kiminsigne@gmail.com>
2018-06-21 18:49:47 -0700

    updated imperfects to use unflipped library

--- scripts/expression_pipeline/map_imperfects.sh
+++ scripts/expression_pipeline/map_imperfects.sh
@@ -1,18 +1,18 @@
 DATA=../../processed_data/expression_pipeline/imperfects
 
-# echo "Generating imperfects..."
-# python generate_imperfects.py ../../ref/endo_lib_2016_controls_clean.txt tab \
-# 24 ${DATA}/endo_lib_imperfects.fasta fasta
+echo "Generating imperfects..."
+python generate_imperfects.py ../../ref/endo_lib_original_seq.txt tab \
+24 ${DATA}/endo_lib_imperfects.fasta fasta
 
-# # set cutoff, otherwise randomly choosing sequences in bootstrapping from large
-# # library takes way too long
-# python barcode_mapping.py ../../processed_data/endo_bc_map_merged_reads_pear/endo_merged.fastq \
-# fastq endo_lib_imperfects.txt tab 150 0 end 20 ${DATA}/endo_mapping_imperfect --cutoff 70
+# set cutoff, otherwise randomly choosing sequences in bootstrapping from large
+# library takes way too long
+python barcode_mapping.py ../../processed_data/endo_bc_map_merged_reads_pear/endo_merged.fastq \
+fastq endo_lib_imperfects.txt tab 150 0 end 20 ${DATA}/endo_mapping_imperfect --cutoff 70
 
 # Rscript bc_counts2expression.R ../../processed_data/expression_pipeline \
 #  ${DATA}/endo_mapping_imperfect_barcode_statistics.txt \
 #  ${DATA}/endo_mapping_imperfect_variant_statistics.txt \
-#  ../../ref/endo_lib_2016_controls_clean.txt ${DATA}/endo_imperfect_expression.txt
+#  ../../ref/endo_lib_imperfects.txt ${DATA}/endo_imperfect_expression.txt
 
 # # predict with uncertainty
 # echo "Predicting..."
@@ -32,7 +32,7 @@ DATA=../../processed_data/expression_pipeline/imperfects
 # sort -k3,3 -rn endo_imperfect_uncertain_predictions.txt > endo_imperfect_uncertain_predictions_sorted.txt
 # mv endo_imperfect_uncertain_predictions_sorted.txt ${DATA}
 
-python generate_imperfect_lib.py \
-${DATA}/endo_imperfect_uncertain_predictions.txt \
-${DATA}/endo_lib_imperfects.fasta \
-60000 ../../lib_gen/20180511_endo_imperfect_lib.txt
+# python generate_imperfect_lib.py \
+# ${DATA}/endo_imperfect_uncertain_predictions.txt \
+# ${DATA}/endo_lib_imperfects.fasta \
+# 60000 ../../lib_gen/20180511_endo_imperfect_lib.txt
