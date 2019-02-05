37cea4a1f7cf8dfb4d67b3569a6c937e2ffcbc24
kinsigne <kiminsigne@gmail.com>
2019-02-05 10:56:31 -0800

    added median

--- scripts/barcode_counts_to_expression.R
+++ scripts/barcode_counts_to_expression.R
@@ -8,11 +8,17 @@ options(scipen = 10000)
 
 # args = commandArgs(trailingOnly=TRUE)
 
-args <- c('../../processed_data/endo_tss/lb',
-          '../../processed_data/endo_tss/lb/endo_lb_bc_map.txt',
-          '../../processed_data/endo_tss/lb/endo_lb_controls_bc_map.txt',
-          '../../ref/endo_lib_2016_controls_clean.txt',
-          '../../processed_data/endo_tss/lb/rLP5_Endo2_lb_expression.txt')
+# args <- c('../../processed_data/endo_tss/lb',
+#           '../../processed_data/endo_tss/lb/endo_lb_bc_map.txt',
+#           '../../processed_data/endo_tss/lb/endo_lb_controls_bc_map.txt',
+#           '../../ref/endo_lib_2016_controls_clean.txt',
+#           '../../processed_data/endo_tss/lb/rLP5_Endo2_lb_expression.txt')
+
+# args <- c('../../processed_data/endo_scramble',
+#           '../../processed_data/endo_scramble/endo_scramble_combined_bc_map.txt',
+#           '../../processed_data/endo_scramble/endo_scramble_combined_controls_bc_map.txt',
+#           '../../ref/20180507_active_tss_scrambled10_stride5.txt',
+#           '../../processed_data/endo_scramble/endo_scramble_expression.txt')
 count_folder <- args[1]
 bc_map_file <- args[2]
 bc_map_file_controls <- args[3]
@@ -110,19 +116,26 @@ if(length(filelist) == 8) {
         mutate(num_barcodes_integrated = n()) %>%
         # filter out promoters with fewer than 3 barcodes integrated
         filter(num_barcodes_integrated >= 3) %>% 
-        mutate(RNA_exp_1_1 = sum(RNA1_1)/(sum(DNA1_1)),
-               RNA_exp_1_2 = sum(RNA1_2)/(sum(DNA1_2)),
-               RNA_exp_2_1 = sum(RNA2_1)/(sum(DNA2_1)),
-               RNA_exp_2_2 = sum(RNA2_2)/(sum(DNA2_2)),
-               RNA_exp_1 = ((RNA_exp_1_1+RNA_exp_1_2)/2),
-               RNA_exp_2 = ((RNA_exp_2_1+RNA_exp_2_2)/2),
-               RNA_exp_ave = ((RNA_exp_1+RNA_exp_2)/2),
-               DNA_1 = (sum(DNA1_1)+sum(DNA1_2)), 
-               DNA_2 = (sum(DNA2_1)+sum(DNA2_2)),  
-               DNA_ave = ((DNA_1 + DNA_2)/2)) %>%
+        mutate(RNA_exp_sum_1_1 = sum(RNA1_1)/(sum(DNA1_1)),
+               RNA_exp_sum_1_2 = sum(RNA1_2)/(sum(DNA1_2)),
+               RNA_exp_sum_2_1 = sum(RNA2_1)/(sum(DNA2_1)),
+               RNA_exp_sum_2_2 = sum(RNA2_2)/(sum(DNA2_2)),
+               RNA_exp_sum_1 = ((RNA_exp_sum_1_1+RNA_exp_sum_1_2)/2),
+               RNA_exp_sum_2 = ((RNA_exp_sum_2_1+RNA_exp_sum_2_2)/2),
+               RNA_exp_sum_ave = ((RNA_exp_sum_1+RNA_exp_sum_2)/2),
+               DNA_1 = (sum(DNA1_1)+sum(DNA1_2)),
+               DNA_2 = (sum(DNA2_1)+sum(DNA2_2)),
+               DNA_ave = ((DNA_1 + DNA_2)/2),
+               expn_1_1 = RNA1_1 / DNA1_1,
+               expn_1_2 = RNA1_2 / DNA1_2,
+               expn_2_1 = RNA2_1 / DNA2_1,
+               expn_2_2 = RNA2_2 / DNA2_2,
+               expn_med1 = median(c(expn_1_1, expn_1_2), na.rm = T),
+               expn_med2 = median(c(expn_2_1, expn_2_2), na.rm = T),
+               expn_med = (expn_med1 + expn_med2)/2) %>%
         ungroup() %>%
-        select(variant, orig_name = name, RNA_exp_1_1, RNA_exp_1_2, RNA_exp_2_1, RNA_exp_2_2,
-               RNA_exp_1, RNA_exp_2, RNA_exp_ave, DNA_1, DNA_2, DNA_ave, 
+        select(variant, orig_name = name, RNA_exp_sum_1_1, RNA_exp_sum_1_2, RNA_exp_sum_2_1, RNA_exp_sum_2_2,
+               RNA_exp_sum_1, RNA_exp_sum_2, RNA_exp_sum_ave, DNA_1, DNA_2, DNA_ave, expn_med,
                num_barcodes_mapped, num_barcodes_integrated) %>% 
         distinct() 
 } else if(length(filelist) == 4) {
@@ -134,10 +147,14 @@ if(length(filelist) == 8) {
         # filter out promoters with fewer than 3 barcodes integrated
         filter(num_barcodes_integrated >= 3) %>% 
         mutate(DNA_sum = (sum(DNA2)+sum(DNA1)),
-               RNA_exp_1 = sum(RNA1)/DNA_sum,
-               RNA_exp_2 = sum(RNA2)/DNA_sum,
-               RNA_exp_ave = ((RNA_exp_1+RNA_exp_2)/2)) %>% 
-        select(variant, orig_name = name, RNA_exp_1, RNA_exp_2, RNA_exp_ave, DNA_sum, 
+               RNA_exp_1 = sum(RNA1)/sum(DNA1),
+               RNA_exp_2 = sum(RNA2)/sum(DNA2),
+               RNA_exp_sum_ave = ((RNA_exp_1+RNA_exp_2)/2),
+               expn_med1 = median(RNA1/DNA1, na.rm = T),
+               expn_med2 = median(RNA2/DNA2, na.rm = T),
+               expn_med = (expn_med1 + expn_med2) / 2) %>% 
+        select(variant, orig_name = name, RNA_exp_1, RNA_exp_2, RNA_exp_sum_ave, DNA_sum, 
+               expn_med1, expn_med2, expn_med, 
                num_barcodes_mapped, num_barcodes_integrated) %>% 
         distinct() 
 }
@@ -147,14 +164,21 @@ print(paste0("Mapped and integrated library: ", round(percent_covered,2) * 100, 
 
 # find threshold - 2 standard deviations greater than median negative control
 negatives <- filter(expression, grepl('neg', orig_name))
-neg_sd <- sd(negatives$RNA_exp_ave)
-neg_median <- median(negatives$RNA_exp_ave, na.rm = T)
-threshold <- 2*neg_sd + neg_median
-print(paste("Active threshold:", threshold))
+neg_sd_sum <- sd(negatives$RNA_exp_sum_ave)
+neg_median_sum <- median(negatives$RNA_exp_sum_ave, na.rm = T)
+threshold_sum <- 2*neg_sd_sum + neg_median_sum
+print(paste("Active threshold (sum):", threshold_sum))
+print("Standardize so threshold = 1")
+
+neg_sd_med <- sd(negatives$expn_med)
+neg_median_med <- median(negatives$expn_med, na.rm = T)
+threshold_med <- 2*neg_sd_med + neg_median_med
+print(paste("Active threshold (median):", threshold_med))
 print("Standardize so threshold = 1")
 
 expression <- expression %>% 
-    mutate(expn_std = RNA_exp_ave - threshold)
+    mutate(expn_sum_std = RNA_exp_sum_ave - threshold_sum,
+           expn_med_std = expn_med - threshold_med)
 
 write.table(expression, output_name, quote = F, row.names = F)
 
